919a9c604dd2d31fac4c1e37d77e1e8e5a65db4f
tebogo60 <tjjmalebana@gmail.com>
2024-11-14 16:16:42 +0200

    all done

--- web_reader.py
+++ web_reader.py
@@ -1,12 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
 
-url = "https://www.youtube.com/"
 
-response = requests.get(url)
+def canConnect(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "html.parser")
-    print(soup.prettify())
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+def searchBarCode(barcode):
+    url = "https://barcode-list.com/barcode/EN/Search.htm?barcode=" + barcode
+
+    if canConnect(url):
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, "html.parser")
+        result = soup.find("p").text
+        print(result)
+
+        table = soup.find("table")
+        readTable(table)
+
+    else:
+        print("Could not connect to url!")
+
+
+def readTable(html_table):
+    table_data = []
+
+    for row in html_table.find_all("tr"):
+        columns = row.find_all(["td", "th"])
+        column_data = [col.text.strip() for col in columns]
+
+        if column_data:
+            table_data.append(column_data)
+
+    try:
+        printTable(table_data)
+    except IndexError:
+        print("Product Unavailable!")
+
+
+def printTable(data):
+    table = PrettyTable()
+
+    if data:
+        table.field_names = data[1]
+        for row in data[2:]:
+            table.add_row(row)
+
+    print(table)
+
+
+def prompting():
+    enter_input = input("Enter barcode: ")
+    searchBarCode(enter_input)
+
+
+prompting()
